d8cfbeca45dbf6308c6420dc7f9b51cef6a3fdcb
mars925 <mars20120925@gmail.com>
2025-03-08 10:33:53 +0800

    class2

---
+++ class2/homework.py
@@ -0,0 +1,15 @@
+"""
+請將下列變數做出正確的型態分類
+apple = 1000
+age = 12
+minor = False
+name = "David"
+weight = 64.5
+zip = "545"
+
+int(整數)  :apple、age
+bool(布林) :minor
+name = "David"
+str(字串)  :name、zip
+float(浮點數、小數):weight
+"""
